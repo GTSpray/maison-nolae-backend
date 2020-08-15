81eed885da93ee82e9aeb9c9670889b6f659a934
GTSpray <69210426+GTSpray@users.noreply.github.com>
2020-08-15 19:16:01 +0200

    add oAuth discord endpoint

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const chalk = require("chalk");
 const { v4: uuidv4 } = require("uuid");
 const favicon = require("serve-favicon");
 const path = require("path");
+const fetch = require("node-fetch");
 
 const apiContracts = require("../contract.js");
 
@@ -41,6 +42,7 @@ app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 app.use((_req, res, next) => {
   res.header("Access-Control-Allow-Origin", config.fronturl);
   res.header("Access-Control-Allow-Methods", "*");
@@ -56,17 +58,72 @@ app.get("/", (_req, res) => {
 });
 
 app.get("/contracts", (_req, res) => {
-  res.send(JSON.stringify(apiContracts));
+  res.json(apiContracts);
 });
 
 app.get("/players", (_req, res) => {
   const players = Array.from(sessions.values()).map((e) => e.player);
-  res.send(JSON.stringify(players));
+  res.json(players);
 });
 
-app.post("/", (req, res) => {
-  console.log(req.body, req.params);
-  res.send("ok");
+app.post("/auth", (req, res) => {
+  if(req.body.code){
+    const data = {
+      client_id: process.env.oauth_discord_client_id,
+      client_secret: process.env.oauth_discord_client_secret,
+      grant_type: "authorization_code",
+      redirect_uri: process.env.oauth_discord_redirect_uri,
+      code: req.body.code,
+      scope: "identify,guilds"
+    };
+
+    fetch("https://discordapp.com/api/oauth2/token", {
+          method: "POST",
+          body: new URLSearchParams(data),
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          }
+        })
+          .then((discordRes) => discordRes.json())
+          .then((info) =>
+            [
+              "https://discordapp.com/api/users/@me",
+              "https://discordapp.com/api/users/@me/guilds"
+            ].map((url) => ({
+              url,
+              opts: {
+                headers: {
+                  authorization: `${info.token_type} ${info.access_token}`
+                }
+              }
+            }))
+          )
+          .then((qs) => Promise.all(qs.map((q) => fetch(q.url, q.opts))))
+          .then((resp) => Promise.all(resp.map((r) => r.json())))
+          .then((infos) => {
+            const [user, guilds] = infos;
+            if (user && guilds) {
+              res.json({
+                pseudo: `${user.username}#${user.discriminator}`,
+                inserver: guilds.some((e) => e.id === process.env.oauth_discord_id_server_discord)
+              })
+            } else {
+              res.json({
+                message: 'error'
+              })
+            }
+          })
+          .catch((e) => {
+            console.error(e);
+            res.json({
+              message: 'error'
+            })
+          });
+      } else {
+        res.json({
+          message: 'no authent'
+        })
+      }
 });
 
 app.ws("/", (ws, req) => {
