1a7fa329b96989165fb92e07669fda6c758fbf27
gtspray <https://github.com/GTSpray/maison-nolae-backend.git>
2020-08-19 00:08:41 +0200

    fix please

---
+++ src/serverLog.js
@@ -0,0 +1,22 @@
+const chalk = require('chalk');
+
+
+function serverLog(data, color, type) {
+  const d = new Date(Date.now());
+  let log = '';
+  data = data.toString().split(/\r?\n/);
+  data.forEach((line) => {
+    log += `  ${line}\n`;
+  });
+  if (/[0-9A-z]+/.test(log)) {
+    console.log(
+      chalk[color].bold(` ┏ ${type} -------------------`) +
+      '\n\n' +
+      log +
+      '\n' +
+      chalk[color].bold(` ┗ ${d.toLocaleString()} ------`) +
+      '\n'
+    );
+  }
+}
+exports.serverLog = serverLog;
