8426d1167c2c19159a9ada6dbd89bdbbec89bead
gtspray <https://github.com/GTSpray/maison-nolae-backend.git>
2020-08-18 20:56:53 +0200

    add linter

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # maison-nolae-backend
 
-![CircleCI](https://circleci.com/gh/GTSpray/maison-nolae-backend/tree/develop.svg?style=svg "CircleCI")
+![CircleCI](https://circleci.com/gh/GTSpray/maison-nolae-backend/tree/master.svg?style=svg "CircleCI")
 
+
+## Installation
+
+```bash
+cp .env.sample .env
+yarn run preset:env
+```
+
+### Linter
+
+This project use [StandardJS](https://standardjs.com) as linter with a pre-commit hook.
+
+To configure text editor, see [this link](https://standardjs.com/#are-there-text-editor-plugins).
+
+To run linter : `yarn run lint`
+
+To run linter with auto-fix : `yarn run lint:fix`
+
+### Tests
+
+`npm test`
--- tests/websocket.spec.js
+++ tests/websocket.spec.js
@@ -1,65 +1,65 @@
-const WebSocket = require("ws");
+const WebSocket = require('ws')
 
 const callSocket = (url, message) =>
   new Promise((resolve) => {
     const ws = new WebSocket(url)
-      .on("open", () => {
+      .on('open', () => {
         ws.send(
-          typeof message === "string" ? message : JSON.stringify(message)
-        );
+          typeof message === 'string' ? message : JSON.stringify(message)
+        )
       })
-      .on("message", (message) => {
-        ws.close();
+      .on('message', (message) => {
+        ws.close()
         try {
-          const json = JSON.parse(message);
+          const json = JSON.parse(message)
           resolve(json)
         } catch (error) {
           resolve(message)
         }
-      });
-  });
-
-describe("Websocket", () => {
-  const url = `ws://localhost:${process.env.PORT}`;
-
-  it("should connect websockets", (done) => {
-    expect.assertions(1);
-    const ws = new WebSocket(url)
-      .on("open", () => {
-        expect(true).toBe(true);
-        ws.close();
       })
-      .on("close", () => done());
-  });
+  })
 
-  it("should respond invalid event when message is not event", async () => {
-    const response = await callSocket(url, {});
-    expect(response).toStrictEqual({ error: "invalid event" });
-  });
+describe('Websocket', () => {
+  const url = `ws://localhost:${process.env.PORT}`
 
-  it("should respond invalid event when message is string", async () => {
-    const response = await callSocket(url, "not json");
-    expect(response).toStrictEqual({ error: "invalid event" });
-  });
+  it('should connect websockets', (done) => {
+    expect.assertions(1)
+    const ws = new WebSocket(url)
+      .on('open', () => {
+        expect(true).toBe(true)
+        ws.close()
+      })
+      .on('close', () => done())
+  })
 
-  it("should respond invalid auth method when authentication.token is bad formated ", async () => {
+  it('should respond invalid event when message is not event', async () => {
+    const response = await callSocket(url, {})
+    expect(response).toStrictEqual({ error: 'invalid event' })
+  })
+
+  it('should respond invalid event when message is string', async () => {
+    const response = await callSocket(url, 'not json')
+    expect(response).toStrictEqual({ error: 'invalid event' })
+  })
+
+  it('should respond invalid auth method when authentication.token is bad formated ', async () => {
     const response = await callSocket(url, {
-      type: "authentication",
+      type: 'authentication',
       payload: {
-        token: "badformated"
+        token: 'badformated'
       }
-    });
-    expect(response).toStrictEqual({ error: "Invalid auth method" });
-  });
+    })
+    expect(response).toStrictEqual({ error: 'Invalid auth method' })
+  })
 
-  it("should respond unable to get session when user use token from another platform", async () => {
+  it('should respond unable to get session when user use token from another platform', async () => {
     const response = await callSocket(url, {
-      type: "authentication",
+      type: 'authentication',
       payload: {
         token:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InRlc3QifQ.z31gNHNwL1V49dzxob6dUackLpNmGCVE9aV9XhaFq7w"
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InRlc3QifQ.z31gNHNwL1V49dzxob6dUackLpNmGCVE9aV9XhaFq7w'
       }
-    });
-    expect(response).toStrictEqual({ error: "Unable to get your session" });
-  });
-});
+    })
+    expect(response).toStrictEqual({ error: 'Unable to get your session' })
+  })
+})
