e83f92372b6fec2ed3f066913f037d882740f0be
gtspray <https://github.com/GTSpray/maison-nolae-backend.git>
2020-08-20 17:51:06 +0200

    lint:fix

--- src/index.js
+++ src/index.js
@@ -1,73 +1,73 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const { v4: uuidv4 } = require("uuid");
-const favicon = require("serve-favicon");
-const path = require("path");
-const fetch = require("node-fetch");
-const { serverLog } = require("./serverLog");
+const express = require('express')
+const bodyParser = require('body-parser')
+const { v4: uuidv4 } = require('uuid')
+const favicon = require('serve-favicon')
+const path = require('path')
+const fetch = require('node-fetch')
+const { serverLog } = require('./serverLog')
 
-const authService = require("./auth.service");
+const authService = require('./auth.service')
 
-const apiContracts = require("../contract.js");
+const apiContracts = require('../contract.js')
 
-const config = require("../.config/endpoint.js");
+const config = require('../.config/endpoint.js')
 
-const Ajv = require("ajv");
-const ajv = Ajv({ allErrors: true });
+const Ajv = require('ajv')
+const ajv = Ajv({ allErrors: true })
 
-const authenticatedUsers = new Map();
+const authenticatedUsers = new Map()
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
 const app = express()
-  .set("port", port)
-  .use(favicon(path.join(__dirname, "public", "favicon.ico")))
+  .set('port', port)
+  .use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
   .use(bodyParser.urlencoded({ extended: true }))
   .use(bodyParser.json())
   .use((_req, res, next) => {
-    res.header("Access-Control-Allow-Origin", config.fronturl);
-    res.header("Access-Control-Allow-Methods", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Max-Age", "1728000");
+    res.header('Access-Control-Allow-Origin', config.fronturl)
+    res.header('Access-Control-Allow-Methods', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    res.header('Access-Control-Max-Age', '1728000')
 
-    next();
+    next()
   })
-  .get("/", (_req, res) => {
+  .get('/', (_req, res) => {
     res.json({
-      message: "Hello Wrold!"
-    });
+      message: 'Hello Wrold!'
+    })
   })
-  .get("/contracts", (_req, res) => {
-    res.json(apiContracts);
+  .get('/contracts', (_req, res) => {
+    res.json(apiContracts)
   })
-  .get("/players", (_req, res) => {
+  .get('/players', (_req, res) => {
     const players = Array.from(authenticatedUsers.values()).map(
       (e) => e.player
-    );
-    res.json(players);
+    )
+    res.json(players)
   })
-  .post("/auth", (req, res) => {
+  .post('/auth', (req, res) => {
     if (req.body.code) {
       const data = {
         client_id: process.env.oauth_discord_client_id,
         client_secret: process.env.oauth_discord_client_secret,
-        grant_type: "authorization_code",
+        grant_type: 'authorization_code',
         redirect_uri: process.env.oauth_discord_redirect_uri,
         code: req.body.code,
-        scope: "identify,guilds"
-      };
+        scope: 'identify,guilds'
+      }
 
       fetch(`${process.env.oauth_discord_base_url}/oauth2/token`, {
-        method: "POST",
+        method: 'POST',
         body: new URLSearchParams(data),
         headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
+          'Content-Type': 'application/x-www-form-urlencoded'
         }
       })
         .then((discordRes) => discordRes.json())
         .then((info) =>
-          ["@me", "@me/guilds"].map((path) => ({
+          ['@me', '@me/guilds'].map((path) => ({
             url: `${process.env.oauth_discord_base_url}/users/${path}`,
             opts: {
               headers: {
@@ -79,7 +79,7 @@ const app = express()
         .then((qs) => Promise.all(qs.map((q) => fetch(q.url, q.opts))))
         .then((resp) => Promise.all(resp.map((r) => r.json())))
         .then((infos) => {
-          const [me, guilds] = infos;
+          const [me, guilds] = infos
           if (
             me &&
             guilds &&
@@ -87,10 +87,10 @@ const app = express()
               (e) => e.id === process.env.oauth_discord_id_server_discord
             )
           ) {
-            const pseudo = `${me.username}#${me.discriminator}`;
+            const pseudo = `${me.username}#${me.discriminator}`
             if (!authenticatedUsers.has(pseudo)) {
-              const id = uuidv4();
-              serverLog(`${pseudo} ${id}`, "green", "WS Server");
+              const id = uuidv4()
+              serverLog(`${pseudo} ${id}`, 'green', 'WS Server')
               authenticatedUsers.set(pseudo, {
                 discord: pseudo,
                 player: {
@@ -100,118 +100,118 @@ const app = express()
                   y: 0
                 },
                 ws: []
-              });
+              })
             }
-            const session = authenticatedUsers.get(pseudo);
+            const session = authenticatedUsers.get(pseudo)
             res.json({
               player: session.player,
               token: authService.issue({
                 id: session.player.id,
                 pseudo
               })
-            });
+            })
           } else {
             res.status(403).json({
-              message: "forbidden"
-            });
+              message: 'forbidden'
+            })
           }
         })
         .catch((e) => {
-          console.error(e);
+          console.error(e)
           res.status(401).json({
-            message: "discord fail"
-          });
-        });
+            message: 'discord fail'
+          })
+        })
     } else {
       res.status(401).json({
-        message: "no authent"
-      });
+        message: 'no authent'
+      })
     }
   })
   .listen(port, () => {
-    serverLog(`Server listening at http://localhost:${port}`, "blue", "Server");
-  });
+    serverLog(`Server listening at http://localhost:${port}`, 'blue', 'Server')
+  })
 
-const { Server } = require("ws");
-const wss = new Server({ server: app });
-const validateWebsocket = ajv.compile(apiContracts.websocket);
-const validatePlayer = ajv.compile(apiContracts.player);
-const validateAuthentication = ajv.compile(apiContracts.authentication);
+const { Server } = require('ws')
+const wss = new Server({ server: app })
+const validateWebsocket = ajv.compile(apiContracts.websocket)
+const validatePlayer = ajv.compile(apiContracts.player)
+const validateAuthentication = ajv.compile(apiContracts.authentication)
 
-wss.on("connection", (ws) => {
-  let session = null;
-  ws.on("close", () => {
+wss.on('connection', (ws) => {
+  let session = null
+  ws.on('close', () => {
     if (session) {
-      session.ws = session.ws.filter((e) => e !== ws);
+      session.ws = session.ws.filter((e) => e !== ws)
       if (session.ws.lengt === 0) {
-        serverLog(`Player quit ${session.player.id}`, "green", "WS Server");
-        authenticatedUsers.delete(session.discord);
+        serverLog(`Player quit ${session.player.id}`, 'green', 'WS Server')
+        authenticatedUsers.delete(session.discord)
       }
     }
-  });
-  ws.on("message", (message) => {
+  })
+  ws.on('message', (message) => {
     try {
-      var event = JSON.parse(message);
-      const valid = validateWebsocket(event);
+      var event = JSON.parse(message)
+      const valid = validateWebsocket(event)
       if (!valid) {
-        throw validateWebsocket.errors[0];
+        throw validateWebsocket.errors[0]
       }
-      ws.emit(event.type, event.payload);
+      ws.emit(event.type, event.payload)
     } catch (error) {
-      console.error("not an event", error);
-      ws.emit("_error", {
-        error: "invalid event"
-      });
+      console.error('not an event', error)
+      ws.emit('_error', {
+        error: 'invalid event'
+      })
     }
   })
 
-    .on("_error", (error) => {
-      ws.send(JSON.stringify(error));
+    .on('_error', (error) => {
+      ws.send(JSON.stringify(error))
     })
 
-    .on("authentication", (payload) => {
-      const valid = validateAuthentication(payload);
+    .on('authentication', (payload) => {
+      const valid = validateAuthentication(payload)
       if (valid) {
         authService.verify(payload.token, (err, decoded) => {
           if (err || !authenticatedUsers.has(decoded.pseudo)) {
-            ws.emit("_error", {
-              error: "Unable to get your session"
-            });
+            ws.emit('_error', {
+              error: 'Unable to get your session'
+            })
           } else {
-            session = authenticatedUsers.get(decoded.pseudo);
-            session.ws.push(ws);
+            session = authenticatedUsers.get(decoded.pseudo)
+            session.ws.push(ws)
           }
-        });
+        })
       } else {
-        console.error(validateAuthentication.errors);
-        ws.emit("_error", {
-          error: "Invalid auth method"
-        });
+        console.error(validateAuthentication.errors)
+        ws.emit('_error', {
+          error: 'Invalid auth method'
+        })
       }
     })
-    .on("player", (payload) => {
+    .on('player', (payload) => {
       if (session) {
-        const valid = validatePlayer(payload);
+        const valid = validatePlayer(payload)
         if (valid) {
-          const id = session.player.id;
+          const id = session.player.id
           session.player = {
             ...payload,
             id
-          };
-          const ps = JSON.stringify(session.player);
+          }
+          const ps = JSON.stringify(session.player)
           authenticatedUsers.forEach((s) => {
             for (const i in s.ws) {
               if (Object.prototype.hasOwnProperty.call(s.ws, i)) {
-                s.ws[i].send(ps);
+                s.ws[i].send(ps)
               }
             }
-          });
+          })
         } else {
-          console.error(session.player.id, validatePlayer.errors);
+          console.error(session.player.id, validatePlayer.errors)
         }
       } else {
-        ws.send("Not authentified session");
-        console.error("Not authentified session");
+        ws.send('Not authentified session')
+        console.error('Not authentified session')
       }
-    });
-});
+    })
+})
--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -1,16 +1,16 @@
-const jose = require("jose");
-const Ajv = require("ajv");
+const jose = require('jose')
+const Ajv = require('ajv')
 expect.extend({
-  toMatchApiContract(body, schema) {
-    const apiContract = new Ajv().addSchema(schema, "apicontract");
-    const pass = !!apiContract.validate("apicontract", body);
+  toMatchApiContract (body, schema) {
+    const apiContract = new Ajv().addSchema(schema, 'apicontract')
+    const pass = !!apiContract.validate('apicontract', body)
     return {
-      message: () => (pass ? "" : apiContract.errorsText(apiContract.errors)),
+      message: () => (pass ? '' : apiContract.errorsText(apiContract.errors)),
       pass
-    };
+    }
   },
-  toMatchJWT(token, expectedPayload) {
-    const payload = jose.JWT.decode(token);
+  toMatchJWT (token, expectedPayload) {
+    const payload = jose.JWT.decode(token)
     return {
       message: () =>
         `expected ${JSON.stringify(
@@ -19,6 +19,6 @@ expect.extend({
           2
         )} JWT token contain ${JSON.stringify(expectedPayload, null, 2)}`,
       pass: this.equals(payload, expect.objectContaining(expectedPayload))
-    };
+    }
   }
-});
+})
