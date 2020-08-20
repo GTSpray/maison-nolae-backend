ead0956214b855ed501a37bbb4e638adac3a6f9a
GTSpray <69210426+GTSpray@users.noreply.github.com>
2020-08-20 13:43:39 +0200

    initial commit

--- tests/http.spec.js
+++ tests/http.spec.js
@@ -1,169 +1,221 @@
-const axios = require('axios').default
-const express = require('express')
-const bodyParser = require('body-parser')
-const contracts = require('../contract')
+const axios = require("axios").default;
+const express = require("express");
+const bodyParser = require("body-parser");
+const contracts = require("../contract");
 const request = async (url, options) => {
-  let response
+  let response;
   try {
-    response = await axios.request(url, options)
+    response = await axios.request(url, options);
   } catch (error) {
     if (error.response) {
-      response = error.response
+      response = error.response;
     } else {
-      throw error
+      throw error;
     }
   }
-  return response
-}
+  return response;
+};
 
-const server = (fakeUrl) => new Promise((resolve) => {
-  const url = new URL(fakeUrl)
-  const spies = {
-    headers: jest.fn(),
-    body: jest.fn(),
-    response: jest.fn()
-  }
-  const fakeApp = express()
-    .use(bodyParser.urlencoded({ extended: true }))
-    .use((req, res) => {
-      spies.headers(req.headers)
-      spies.body(req.body)
-      spies.response(req, res)
-    })
+const server = (fakeUrl) =>
+  new Promise((resolve) => {
+    const url = new URL(fakeUrl);
+    const spies = {
+      headers: jest.fn(),
+      body: jest.fn(),
+      response: jest.fn()
+    };
+    const fakeApp = express()
+      .use(bodyParser.urlencoded({ extended: true }))
+      .use((req, res) => {
+        spies.headers(req.headers);
+        spies.body(req.body);
+        spies.response(req, res);
+      });
 
-  const server = fakeApp.listen(url.port, () => {
-    resolve({
-      url,
-      spies,
-      server
-    })
-  })
-})
+    const server = fakeApp.listen(url.port, () => {
+      resolve({
+        url,
+        spies,
+        server
+      });
+    });
+  });
 
-describe('HTTP Server', () => {
-  const url = `http://localhost:${process.env.PORT}`
+describe("HTTP Server", () => {
+  const url = `http://localhost:${process.env.PORT}`;
 
-  it('get / should return hello world', async () => {
-    const response = await axios.get(url)
-    expect(response.status).toBe(200)
+  it("get / should return hello world", async () => {
+    const response = await axios.get(url);
+    expect(response.status).toBe(200);
     expect(response.data).toStrictEqual({
-      message: 'Hello Wrold!'
-    })
-  })
+      message: "Hello Wrold!"
+    });
+  });
 
-  it('get /contracts should return list of contracts', async () => {
-    const response = await axios.get(`${url}/contracts`)
-    expect(response.status).toBe(200)
-    expect(response.data).toStrictEqual(contracts)
-  })
+  it("get /contracts should return list of contracts", async () => {
+    const response = await axios.get(`${url}/contracts`);
+    expect(response.status).toBe(200);
+    expect(response.data).toStrictEqual(contracts);
+  });
 
-  it('get /player should return list of players', async () => {
-    const response = await axios.get(`${url}/players`)
-    expect(response.status).toBe(200)
-    expect(response.data).toStrictEqual([])
+  it("get /player should return list of players", async () => {
+    const response = await axios.get(`${url}/players`);
+    expect(response.status).toBe(200);
+    expect(response.data).toStrictEqual([]);
   });
 
   [
-    ['/', 'GET'],
-    ['/contracts', 'GET'],
-    ['/players', 'GET'],
-    ['/auth', 'POST']
+    ["/", "GET"],
+    ["/contracts", "GET"],
+    ["/players", "GET"],
+    ["/auth", "POST"]
   ].forEach(([path, method]) => {
     const corsHeaders = {
-      'access-control-allow-origin': process.env.fronturl,
-      'access-control-allow-methods': '*',
-      'access-control-allow-headers': '*',
-      'access-control-max-age': '1728000'
-    }
+      "access-control-allow-origin": process.env.fronturl,
+      "access-control-allow-methods": "*",
+      "access-control-allow-headers": "*",
+      "access-control-max-age": "1728000"
+    };
 
     it(`${method} ${path} should add headers to prevent CORS failure`, async () => {
-      const response = await request(`${url}${path}`, { method })
-      expect(response.headers).toEqual(expect.objectContaining(corsHeaders))
-    })
+      const response = await request(`${url}${path}`, { method });
+      expect(response.headers).toEqual(expect.objectContaining(corsHeaders));
+    });
 
     it(`get ${path} should add headers to prevent CORS failure`, async () => {
-      const response = await axios.options(`${url}${path}`)
-      expect(response.status).toBe(200)
-      expect(response.headers).toEqual(expect.objectContaining(corsHeaders))
-    })
-  })
+      const response = await axios.options(`${url}${path}`);
+      expect(response.status).toBe(200);
+      expect(response.headers).toEqual(expect.objectContaining(corsHeaders));
+    });
+  });
 
-  describe('/auth', () => {
-    let fakeServer
+  describe("/auth", () => {
+    let fakeServer;
     beforeAll(async () => {
-      fakeServer = await server(process.env.oauth_discord_base_url)
-    })
+      fakeServer = await server(process.env.oauth_discord_base_url);
+    });
 
     afterEach(() => {
-      jest.clearAllMocks()
-    })
+      jest.clearAllMocks();
+    });
 
     afterAll((done) => {
-      fakeServer.server.close(done)
-    })
+      fakeServer.server.close(done);
+    });
 
-    describe('should call discord oAuth service', () => {
+    describe("should call discord oAuth service", () => {
       const options = {
-        method: 'POST',
+        method: "POST",
         data: {
           code: 12345
         }
-      }
+      };
 
-      it('for getting user token respond 401 discord authent fail', async () => {
-        fakeServer.spies.response.mockImplementation((_req, res) => res.status(401).send())
+      it("for getting user token respond 401 discord authent fail", async () => {
+        fakeServer.spies.response.mockImplementation((_req, res) =>
+          res.status(401).send()
+        );
 
-        const response = await request(`${url}/auth`, options)
-        expect(response.status).toBe(401)
+        const response = await request(`${url}/auth`, options);
+        expect(response.status).toBe(401);
         expect(response.data).toStrictEqual({
-          message: 'discord fail'
-        })
+          message: "discord fail"
+        });
 
-        expect(fakeServer.spies.response).toHaveBeenCalled()
+        expect(fakeServer.spies.response).toHaveBeenCalledTimes(1);
         expect(fakeServer.spies.body).toHaveBeenCalledWith({
           client_id: process.env.oauth_discord_client_id,
           client_secret: process.env.oauth_discord_client_secret,
-          grant_type: 'authorization_code',
+          grant_type: "authorization_code",
           redirect_uri: process.env.oauth_discord_redirect_uri,
           code: options.data.code.toString(),
-          scope: 'identify,guilds'
-        })
+          scope: "identify,guilds"
+        });
         expect(fakeServer.spies.headers).toHaveBeenCalledWith({
-          accept: '*/*',
-          'accept-encoding': 'gzip,deflate',
-          connection: 'close',
-          'content-length': '184',
-          'content-type': 'application/x-www-form-urlencoded',
+          accept: "*/*",
+          "accept-encoding": "gzip,deflate",
+          connection: "close",
+          "content-length": "184",
+          "content-type": "application/x-www-form-urlencoded",
           host: `${fakeServer.url.hostname}:${fakeServer.url.port}`,
-          'user-agent': 'node-fetch/1.0 (+https://github.com/bitinn/node-fetch)'
-        })
-      })
+          "user-agent": "node-fetch/1.0 (+https://github.com/bitinn/node-fetch)"
+        });
+      });
 
-      it('and return user jwt when all step are done', async () => {
-        fakeServer.spies.response.mockImplementationOnce((_req, res) => {
-          res.status(200).send({
-            access_token: '6qrZcUqja7812RVdnEKjpzOL4CvHBFG',
-            token_type: 'Bearer',
-            expires_in: 604800,
-            refresh_token: 'D43f5y0ahjqew82jZ4NViEr2YafMKhue',
-            scope: 'identify,guilds'
+      it("for getting user token respond 401 when user is not in server", async () => {
+        const userDiscord = {
+          username: "johnnyKnoxville",
+          discriminator: "12345"
+        };
+        fakeServer.spies.response
+          .mockImplementationOnce((_req, res) => {
+            res.status(200).send({
+              access_token: "6qrZcUqja7812RVdnEKjpzOL4CvHBFG",
+              token_type: "Bearer",
+              expires_in: 604800,
+              refresh_token: "D43f5y0ahjqew82jZ4NViEr2YafMKhue",
+              scope: "identify,guilds"
+            });
           })
-        })
-          .mockImplementationOnce((_req, res) => res.status(200).send({
-            username: 'Johnny Noxville',
-            discriminator: '12345'
-          }))
-          .mockImplementationOnce((_req, res) => res.status(200).send(
-            [
-              { id: process.env.oauth_discord_id_server_discord }
-            ]
-          ))
+          .mockImplementationOnce((_req, res) =>
+            res.status(200).json(userDiscord)
+          )
+          .mockImplementationOnce((_req, res) =>
+            res.status(200).json([{ id: "anotherone" }, { id: "bitedadust" }])
+          );
 
-        const response = await request(`${url}/auth`, options)
-        expect(response.status).toBe(200)
-        expect(response.data).toHaveProperty('player')
-        expect(response.data).toHaveProperty('token')
-      })
-    })
-  })
-})
+        const response = await request(`${url}/auth`, options);
+
+        expect(fakeServer.spies.response).toHaveBeenCalledTimes(3);
+        expect(response.status).toBe(403);
+        expect(response.data).toStrictEqual({
+          message: "forbidden"
+        });
+      });
+
+      it("and return user jwt when all step are done", async () => {
+        const userDiscord = {
+          username: "johnnyKnoxville",
+          discriminator: "12345"
+        };
+        fakeServer.spies.response
+          .mockImplementationOnce((_req, res) => {
+            res.status(200).send({
+              access_token: "6qrZcUqja7812RVdnEKjpzOL4CvHBFG",
+              token_type: "Bearer",
+              expires_in: 604800,
+              refresh_token: "D43f5y0ahjqew82jZ4NViEr2YafMKhue",
+              scope: "identify,guilds"
+            });
+          })
+          .mockImplementationOnce((_req, res) =>
+            res.status(200).json(userDiscord)
+          )
+          .mockImplementationOnce((_req, res) =>
+            res
+              .status(200)
+              .json([
+                { id: "another" },
+                { id: process.env.oauth_discord_id_server_discord }
+              ])
+          );
+
+        const response = await request(`${url}/auth`, options);
+        expect(response.status).toBe(200);
+        expect(response.data).toEqual({
+          player: {
+            id: expect.anything(),
+            pseudo: userDiscord.username,
+            x: 0,
+            y: 0
+          },
+          token: expect.anything()
+        });
+        expect(response.data.token).toMatchJWT({
+          id: response.data.player.id,
+          pseudo: `${userDiscord.username}#${userDiscord.discriminator}`
+        });
+      });
+    });
+  });
+});
