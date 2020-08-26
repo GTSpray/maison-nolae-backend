414069bb93af01d0acbaeebb1a0bbe2b2e71ac5f
gtspray <https://github.com/GTSpray/maison-nolae-backend.git>
2020-08-26 19:38:07 +0200

    lint fix

--- set_env.js
+++ set_env.js
@@ -12,10 +12,10 @@ while (m !== null) {
   m = regex.exec(envSample)
 }
 try {
-  const content =  newLines.join('\n')
-    // .replace(/\r\n/gm, "\n")    //normalize
-    // .replace(/\n/gm,   "\r\n")  //CR+LF  -  Windows EOL
-  fs.writeFileSync('.env', content, {flag:"w", encoding:"utf8"}); 
+  const content = newLines.join('\n')
+  // .replace(/\r\n/gm, "\n")    //normalize
+  // .replace(/\n/gm,   "\r\n")  //CR+LF  -  Windows EOL
+  fs.writeFileSync('.env', content, { flag: 'w', encoding: 'utf8' })
 } catch (err) {
   // An error occurred
   console.error(err)
