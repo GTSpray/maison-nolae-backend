32c0b84d129ae970e9cbda7c86f31d59b7581d67
gtspray <https://github.com/GTSpray/maison-nolae-backend.git>
2020-08-26 08:48:53 +0200

    add more realist mock datas

--- tests/helpers/http.helper.js
+++ tests/helpers/http.helper.js
@@ -48,33 +48,55 @@ module.exports.server = (fakeUrl) =>
   })
 
 module.exports.randomAuth = async (oAuthS, url) => {
-  const mockUser = {
-    username: 'randomUser' + randomStringNumber(5),
-    discriminator: randomStringNumber(4)
-  }
-  const mockToken = {
-    access_token: randomStringNumber(10),
-    token_type: 'Bearer',
-    expires_in: 604800,
-    refresh_token: randomStringNumber(10),
-    scope: 'identify,guilds'
+  const mockEndPoint = {
+    'oauth2/token': {
+      access_token: randomStringNumber(10),
+      token_type: 'Bearer',
+      expires_in: 604800,
+      refresh_token: randomStringNumber(10),
+      scope: 'identify,guilds'
+    },
+    'users/@me': {
+      id: randomStringNumber(18),
+      username: 'randomUser' + randomStringNumber(5),
+      avatar: randomStringNumber(32),
+      discriminator: randomStringNumber(4),
+      public_flags: 0,
+      flags: 0,
+      locale: 'en',
+      mfa_enabled: false
+    },
+    'users/@me/guilds': [
+      {
+        id: randomStringNumber(18),
+        name: `randomServer ${randomStringNumber(5)}`,
+        icon: randomStringNumber(32),
+        owner: false,
+        permissions: parseInt('1' + randomStringNumber(8), 10),
+        features: ['ANIMATED_ICON', 'INVITE_SPLASH'],
+        permissions_new: parseInt('1' + randomStringNumber(8), 10)
+      },
+      {
+        id: process.env.oauth_discord_id_server_discord,
+        name: `Server ${randomStringNumber(5)}`,
+        icon: randomStringNumber(32),
+        owner: false,
+        permissions: '1' + randomStringNumber(8),
+        features: ['ANIMATED_ICON', 'INVITE_SPLASH'],
+        permissions_new: '1' + randomStringNumber(8)
+      }
+    ]
   }
 
   oAuthS.response.mockClear()
   oAuthS.response
-    .mockImplementationOnce((_req, res) => {
-      res.status(200).json(mockToken)
-    })
-    .mockImplementationOnce((_req, res) => res.status(200).json(mockUser))
-    .mockImplementationOnce((_req, res) =>
-      res
-        .status(200)
-        .json([
-          { id: process.env.oauth_discord_id_server_discord }
-        ])
-    )
+    .mockImplementationOnce((_req, res) => res.json(mockEndPoint['oauth2/token']))
+    .mockImplementationOnce((_req, res) => res.json(mockEndPoint['users/@me']))
+    .mockImplementationOnce((_req, res) => res.json(mockEndPoint['users/@me/guilds']))
+
   const response = await axios.post(url, {
     code: randomStringNumber()
   })
+
   return response.data
 }
