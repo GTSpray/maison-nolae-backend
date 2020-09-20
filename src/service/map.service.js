c6da29308a0187d7f41bea4a89e7f0dc9c617ad5
gtspray <https://github.com/GTSpray/maison-nolae-backend.git>
2020-09-20 19:53:49 +0200

    wip room

--- src/service/map.service.js
+++ src/service/map.service.js
@@ -1,4 +1,9 @@
-var archifacile = require('./loadPlan.json')
+var archifacile = require('./loadPlan2.json')
+
+var jsdom = require('jsdom')
+const { JSDOM } = jsdom
+
+const d3 = require('d3')
 
 function getRandomColor () {
   const letters = '0123456789ABCDEF'
@@ -9,26 +14,74 @@ function getRandomColor () {
   return color
 }
 
-var jsdom = require('jsdom')
-const { JSDOM } = jsdom
-
-const d3 = require('d3')
-
 const dom = new JSDOM('<!DOCTYPE html><body></body>')
 
 const body = d3.select(dom.window.document.querySelector('body'))
 const svg = body.append('svg').attr('xmlns', 'http://www.w3.org/2000/svg')
+
 const { minX, minY, maxX, maxY } = parseWall(archifacile, svg)
 
 parseHole(archifacile, svg)
 
+const toD = d3.line()
+  .x((d) => d.x)
+  .y((d) => d.y)
+
 for (const plan of archifacile.data.plan.plans) {
   for (const room of plan.pieces) {
-    svg.selectAll(`.wall.ofroom${room.id}`).style('stroke', getRandomColor())
+    const boundaries = []
+    svg.selectAll(`line.boundary[room="${room.id}"`)
+      .each(function () {
+        const boundary = d3.select(this)
+        boundaries.push({
+          x1: parseInt(boundary.attr('x1'), 10),
+          y1: parseInt(boundary.attr('y1'), 10),
+          x2: parseInt(boundary.attr('x2'), 10),
+          y2: parseInt(boundary.attr('y2'), 10)
+        })
+      })
+    if (boundaries.length > 0) {
+      const paths = []
+      for (const boundary of boundaries) {
+        const path = []
+        let next = boundary
+        while (next && (!next.pass || next === boundary)) {
+          path.push({
+            x: next.x1,
+            y: next.y1
+          })
+          next.pass = true
+          next = boundaries
+            .find(b => (
+              next.x2 === b.x1 && next.y2 === b.y1
+            ))
+        }
+        path.push({
+          x: boundary.x2,
+          y: boundary.y2
+        })
+
+        if (path.length > 1) {
+          paths.push(path)
+        }
+      }
+
+      if (paths.length > 0) {
+        const d = paths.reduce((d, path) => `${d} ${toD(path)}`, '')
+        svg.append('path')
+          .attr('d', d)
+          .attr('class', 'room')
+          .attr('id', `room${room.id}`)
+          .style('stroke', getRandomColor())
+          .style('stroke-width', 100)
+          .style('fill', 'none')
+      }
+    }
   }
 }
 
-svg.style('width', '100%')
+svg.style('maw-width', '100%')
+svg.style('maw-height', '100%')
 svg.attr('viewBox', `${minX} ${minY} ${maxX - minX} ${maxY - minY}`)
 
 function parseHole (houseDescription, svg) {
@@ -36,12 +89,12 @@ function parseHole (houseDescription, svg) {
     for (const hole of plan.trous) {
       const wall = svg.select(`#wall${hole.imur}`)
 
-      const direction = wall.attr('direction')
+      const direction = parseFloat(wall.attr('direction'))
       const cos = Math.cos(direction)
       const sin = Math.sin(direction)
 
-      const x1 = wall.attr('x1') - cos * (hole.dcoin1 + hole.large / 2)
-      const y1 = wall.attr('y1') - sin * (hole.dcoin1 + hole.large / 2)
+      const x1 = parseInt(wall.attr('x1'), 10) - cos * (hole.dcoin1 + hole.large / 2)
+      const y1 = parseInt(wall.attr('y1'), 10) - sin * (hole.dcoin1 + hole.large / 2)
       const x2 = x1 + cos * hole.large
       const y2 = y1 + sin * hole.large
 
@@ -64,6 +117,7 @@ function parseWall (houseDescription, svg) {
   let minY = Infinity
   let maxY = -Infinity
   for (const plan of houseDescription.data.plan.plans) {
+    const g = svg.append('g').attr('id', `plan${plan.etage}`)
     for (const wall of plan.murs) {
       minX = Math.min(minX, wall.x1 - wall.epais, wall.x2 - wall.epais)
       minY = Math.min(minY, wall.y1 - wall.epais, wall.y2 - wall.epais)
@@ -71,28 +125,39 @@ function parseWall (houseDescription, svg) {
       maxX = Math.max(maxX, wall.x1 + wall.epais, wall.x2 + wall.epais)
       maxY = Math.max(maxY, wall.y1 + wall.epais, wall.y2 + wall.epais)
 
-      wall.color = 'orange'
+      const color = 'orange'
+      for (const boundary of wall.cote) {
+        g.append('line')
+          .attr('x1', wall.x1)
+          .attr('y1', wall.y1)
+          .attr('x2', wall.x2)
+          .attr('y2', wall.y2)
+          .attr('room', boundary.iPiece)
+          .attr('class', 'boundary')
 
-      const direction = Math.atan2(wall.y1 - wall.y2, wall.x1 - wall.x2)
+        g.append('line')
+          .attr('x1', wall.x2)
+          .attr('y1', wall.y2)
+          .attr('x2', wall.x1)
+          .attr('y2', wall.y1)
+          .attr('room', boundary.iPiece)
+          .attr('class', 'boundary')
+      }
 
-      svg
-        .append('line')
+      g.append('line')
         .attr('id', `wall${wall.id}`)
         .attr('x1', wall.x1)
         .attr('y1', wall.y1)
         .attr('x2', wall.x2)
         .attr('y2', wall.y2)
-        .attr('epais', wall.epais)
-        .attr('direction', direction)
+        .attr('direction', Math.atan2(wall.y1 - wall.y2, wall.x1 - wall.x2))
+        .attr('class', 'wall')
         .style('stroke-width', wall.epais)
-        .style('stroke', wall.color)
+        .style('stroke', color)
         .style('stroke-linecap', 'square')
-        .attr(
-          'class',
-          `wall ${wall.cote.map((e) => `ofroom${e.iPiece}`).join(' ')}`
-        )
     }
   }
+
   return {
     minX,
     minY,
