4023ac900d3b9f5674244de75c34d3a1bfa85dbc
gtspray <https://github.com/GTSpray/maison-nolae-backend.git>
2020-10-05 17:31:30 +0200

    connect archifacile api with fetch

--- src/controller/map.controller.js
+++ src/controller/map.controller.js
@@ -1,11 +1,20 @@
-const mapService = require('../service/map.service')
-const mapDescription = require('../../tests/mockdatas/nolae-house.json')
+const fetch = require('node-fetch')
 
-const svgMap = mapService.getMap(mapDescription)
+const mapService = require('../service/map.service')
+let mapDescription
+let svgMap
 
 module.exports = {
-  getMap: (_req, res) => {
+  getMap: async (_req, res) => {
     res.setHeader('Content-Type', 'image/svg+xml')
+
+    if (!mapDescription || svgMap) {
+      console.log('get')
+      const getDesc = await fetch(`https://www.archifacile.fr/api/loadPlan?cle=${process.env.archifacile_plan_id}`)
+      mapDescription = await getDesc.json()
+      svgMap = mapService.getMap(mapDescription)
+    }
+
     res.send(svgMap)
   }
 }
